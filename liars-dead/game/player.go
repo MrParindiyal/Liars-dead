08f8c2472369a574ac817b932ea5c21c54a8c791
AP <136710760+MrParindiyal@users.noreply.github.com>
2026-06-17 23:04:10 +0000

    [FEAT] Create new Player with given Name and PlayerId

--- liars-dead/game/player.go
+++ liars-dead/game/player.go
@@ -1,1 +1,22 @@
 package game
+
+import "crypto/rand"
+
+type Player struct {
+	Uid         string
+	PlayerId    int
+	Name        string
+	Hand        []Card
+	Lives       int
+	IsSpectator bool
+}
+
+func NewPlayer(name string, playerNum int) Player {
+	return Player{
+		Uid:         (rand.Text())[:6],
+		PlayerId:    playerNum,
+		Name:        name,
+		Lives:       6,
+		IsSpectator: false,
+	}
+}
