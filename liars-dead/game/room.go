cb750af4271a92fe0329522f48ff1b4bb20d5d54
AP <136710760+MrParindiyal@users.noreply.github.com>
2026-06-18 13:20:46 +0000

    [FEAT] Create GameRoom, add Players to table and compute active players

--- liars-dead/game/room.go
+++ liars-dead/game/room.go
@@ -1,1 +1,40 @@
 package game
+
+// import "crypto/rand"
+
+type GameRoom struct {
+	Id                string
+	Players           []*Player
+	GameMode          string
+	MaxAllowedPlayers int
+	PlayedStack       []Card
+	nextSeatId        int
+}
+
+func NewGameRoom(mode string, maxPlayers int) *GameRoom {
+	return &GameRoom{
+		// Id: rand.Text()[2:8],  // TODO : how to assign room id?
+		GameMode:          mode,
+		MaxAllowedPlayers: maxPlayers,
+	}
+}
+
+func (r *GameRoom) AddPlayerToTable(p *Player) {
+	if len(r.ActivePlayers()) >= r.MaxAllowedPlayers {
+		p.MarkSpectator()
+	}
+	seatId := r.nextSeatId
+	p.SeatId = seatId
+	r.Players = append(r.Players, p)
+	r.nextSeatId++
+}
+
+func (r *GameRoom) ActivePlayers() []*Player {
+	var active []*Player
+	for _, p := range r.Players {
+		if !p.IsSpectator {
+			active = append(active, p)
+		}
+	}
+	return active
+}
