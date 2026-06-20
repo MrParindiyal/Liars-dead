1dfa07e510164f06b70ea0d416feb75eada86e17
AP <136710760+MrParindiyal@users.noreply.github.com>
2026-06-20 07:24:36 +0000

    [CHORE] Temporary tests for game room logics

--- liars-dead/main.go
+++ liars-dead/main.go
@@ -11,8 +11,9 @@ func main() {
 	deck = game.ShuffleDeck(deck)
 	fmt.Println("---------------------")
 
-	hands := game.DealNHands(deck, 3)
-	for x := range 3 {
+	hands := game.DealNHands(deck, 6)
+	room := game.NewGameRoom("devil", 4)
+	for x := range 6 {
 		playerId := rand.Text()[:7]
 		player := game.NewPlayer(fmt.Sprintf("player%d", x), playerId)
 		player.Hand = hands[x].Cards
@@ -24,5 +25,13 @@ func main() {
 		fmt.Println(player.Lives)
 		fmt.Println(player.IsSpectator)
 		fmt.Println("===========")
+		room.AddPlayerToTable(player)
 	}
+
+	fmt.Println(room.Players[0])
+	fmt.Println(room.Players[1])
+	fmt.Println(room.Players[2])
+	fmt.Println(room.Players[3])
+	fmt.Println(room.Players[4])
+	fmt.Println(room.Players[5])
 }
