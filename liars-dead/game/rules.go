d0ceb8323e175f0b21d2a62e4710cf61265833c3
AP <136710760+MrParindiyal@users.noreply.github.com>
2026-07-05 03:36:50 +0000

    [FEAT] Initial implementation of rules/actions

--- liars-dead/game/rules.go
+++ liars-dead/game/rules.go
@@ -1,7 +1,33 @@
 package game
 
 func LegalActions(r *GameRoom, p *Player) []Action {
-	return nil // TODO : implement game logic & rules
+
+	switch r.State {
+	case StateLobby:
+		return []Action{{ActionType: "ready"}, {ActionType: "leave"}}
+	case StateCountdown:
+		return []Action{}
+	case StateInRound:
+		if p.IsSpectator {
+			return []Action{{ActionType: "leave"}}
+		}
+		if r.CurrentTurnSeat != p.SeatId {
+			return []Action{{ActionType: "leave"}}
+		}
+		var legal []Action
+
+		// TODO : implement legal actions
+
+		return legal
+
+	case StateRoundEnd:
+		return []Action{{ActionType: "leave"}}
+	case StateGameOver:
+		return []Action{{ActionType: "leave"}}
+	default:
+		return []Action{}
+	}
+
 }
 
 func ApplyAction(r *GameRoom, p *Player, a Action) error {
